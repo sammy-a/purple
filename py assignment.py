90d315e119c41635913dd406f2ffffb318edbcbd
NWOKOBIA ESTHER <esthernwokobia@gmail.com>
2020-03-26 11:32:06 +0100

    python assignment

---
+++ py assignment.py
@@ -0,0 +1,9 @@
+# Python Program to Calculate the Area of Circle
+
+PI = 3.14
+radius = float(input("Please enter the radius:"))
+area = PI * radius * radius
+
+print("Area of a Circle = %0.2f" %area)
+
+input("hit any key to close")
