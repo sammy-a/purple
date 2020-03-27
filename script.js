b8da1ff27de7645d26fb6787537d9042634a1e51
sammy-a <59881296+sammy-a@users.noreply.github.com>
2020-03-27 14:45:32 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let name ="Nwokobia Esther";
+        console.log("My name is " + name);
+        var courses = [ "HTML", "CSS", "JavaScript" ];
+    for (let i=1; i<=200; i++)
+        {
+            if (i % 2 !== 0)
+            {
+                console.log(i) ;
+            }
+        }
+        
